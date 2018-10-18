9b2b6c36e6d0d88be0fa371885e9642e5daf16e6
denchikUniversal <tkachenkodenys94@gmail.com>
2018-10-18 13:29:44 +0300

    bug fix#1

--- src/components/SourceSelection.vue
+++ src/components/SourceSelection.vue
@@ -1,43 +1,50 @@
 <template>
-    <div class="sourceselection">
+  <div class="sourceselection">
+    <div>
+      <div class="jumbotron">
+        <h2><span class="glyphicon glyphicon-list-alt"></span>&nbsp;News List</h2>
         <h4>Select News Source</h4>
-        <select class="form-control" v-on:="sourceChanged">
-            <option 
-            v-bind:value="source.id" 
-            v-for="(source, i) in sources" :key="i">{{source.name}}</option>
+        <select class="form-control" v-on:change="sourceChanged">
+          <option value="">Please select news source ...</option>
+          <option v-for="(source,id) in sources" v-bind:value="source.id" :key="id">{{source.name}}</option>
         </select>
-    </div>    
+        <div v-if="source">
+          <h6>{{source.description}}</h6>
+          <a v-bind:href="source.url" class="btn btn-primary" target="_blank">Go To {{source.name}} Website</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'sourceselection',
-    data() {
-        return {
-            sources: [],
-            source: ''
-        }
-    },
-    methods: {
-        sourceChanged: function(e) {
-            for (var i = 0; i < this.sources.lenght; i++) {
-                if (this.sources[i].id == e.tarhet.value) {
-                    this.source = this.source[i];
-                }
-            }
-        }
-    },
-    created: function() {
-        this.$http.get('https://newsapi.org/v2/sources?&apiKey=4370ceb1a6fe4e1ba693a36ce793609b')
-            .then(response => {
-                this.sources = response.data.sources
-            })
+  name: 'sourceselection',
+  data () {
+    return {
+      sources: [],
+      source: ''
     }
+  },
+  methods: {
+    sourceChanged: function(e) {
+     for (var i=0; i<this.sources.length; i++) {
+       if (this.sources[i].id == e.target.value) {
+         this.source = this.sources[i];
+       }
+     }
+     this.$emit('sourceChanged', e.target.value);
+    }
+  },
+  created: function () {
+    this.$http.get('https://newsapi.org/v1/sources?language=en')
+      .then(response => {
+        this.sources = response.data.sources;
+      });
+  }
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
-
-
